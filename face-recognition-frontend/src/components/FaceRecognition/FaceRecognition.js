f22555519a556811f8da6dde67f04029efa31ce6
Kathy Su <66896340+kathy-su@users.noreply.github.com>
2021-05-10 19:57:09 -0700

    Add files via upload

--- face-recognition-frontend/src/components/FaceRecognition/FaceRecognition.js
+++ face-recognition-frontend/src/components/FaceRecognition/FaceRecognition.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import './FaceRecognition.css';
 
+const FaceRecognition = ({ imageUrl, box }) => {
+  const faces = box.map((face, j) => {return <div key={j} className='bounding-box' style={ {top: face?.topRow, right: face?.rightCol, bottom: face?.bottomRow, left: face?.leftCol} }> </div>});
+  return (
+    <div className='center ma'>
+      <div className='absolute mt2'>
+        <img id='inputImage' alt='' src={imageUrl} width='500px' height='auto'/>
+        {faces}
+      </div>
+    </div>
+  );
+}
+
+export default FaceRecognition;
