a0789469a2fcf9ba77c07b098524dd7735caad24
Kathy Su <66896340+kathy-su@users.noreply.github.com>
2021-05-13 09:30:52 -0700

    Update App.js

--- face-recognition-frontend/src/App.js
+++ face-recognition-frontend/src/App.js
@@ -11,7 +11,8 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Clarifai from 'clarifai';
 
 const app = new Clarifai.App({
-  apiKey: "cafc43440869401694e252775a06914e",
+  //enter your own clarifai api key
+  apiKey: "",
  });
 
  const particlesOptions = {
@@ -25,24 +26,25 @@ const app = new Clarifai.App({
     }
   }
 }
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: [],
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: [],
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -83,15 +85,6 @@ class App extends Component {
     this.setState({imageUrl: this.state.input});
     app.models
       .predict(
-        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-        // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
-        // so you would change from:
-        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        // to:
-        // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
       .then(response => {
@@ -107,7 +100,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count}))
             })
-
+            .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
@@ -116,7 +109,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
