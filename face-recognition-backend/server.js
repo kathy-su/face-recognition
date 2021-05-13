d533a705e960f1f4606288d2acf8e226024e53e1
Kathy Su <66896340+kathy-su@users.noreply.github.com>
2021-05-13 10:52:15 -0700

    fixing register/signup issues

--- face-recognition-backend/server.js
+++ face-recognition-backend/server.js
@@ -25,13 +25,17 @@ const db = knex ({
   })
   
   app.post('/signin', (req, res) => {
+    const { email, password } = req.body;
+    if (!email||!password){
+      return res.status(400).json('incorrect form submission')
+    }
     db.select('email', 'hash').from('login')
-      .where('email', '=', req.body.email)
+      .where('email', '=', email)
       .then(data => {
-        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        const isValid = bcrypt.compareSync(password, data[0].hash);
         if (isValid) {
           return db.select('*').from('users')
-            .where('email', '=', req.body.email)
+            .where('email', '=', email)
             .then(user => {
               res.json(user[0])
             })
@@ -45,6 +49,9 @@ const db = knex ({
   
   app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+      if (!email||!name||!password){
+        return res.status(400).json('incorrect form submission')
+      }
     const hash = bcrypt.hashSync(password);
       db.transaction(trx => {
         trx.insert({
@@ -102,11 +109,9 @@ const db = knex ({
   })
 
 /*
-
 / -- > res = this is working
 /signin --> POST = success/fail
 /register --> POST = user
 /profile/:userId --> GET = user
 /image --> PUT --> user
-
 */
